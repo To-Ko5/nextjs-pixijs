54620b03656264bdb303aaddced7bed425f167d6
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2022-07-22 18:25:29 +0900

    Add: canvas html

--- pages/filter.tsx
+++ pages/filter.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
 
 const Filter = () => {
-  return <div>filter</div>
+  return (
+    <>
+      <div
+        id="main"
+        style={{
+          height: '100%',
+          width: '100%',
+          maxHeight: '1200px',
+          position: 'fixed',
+          top: 0,
+          left: 0
+        }}
+      ></div>
+    </>
+  )
 }
 
 export default Filter
